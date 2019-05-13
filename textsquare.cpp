f7ea58e051df7e03beb2b99364cb87e48985392e
Kiiruuu <anonymous.kiiruuu@gmail.com>
2019-05-14 03:44:17 +0800

    Create textsquare.cpp

---
+++ textsquare.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+
+int main() { 
+    char input[47];
+    std::cout << "Type anything of up to 46 characters: ";
+    std::cin >> input;
+    std::cout << "\n\n";
+
+    std::cout << input << std::endl;
+    if(strlen(input) <= 46)  {
+        for(int i=1; i < strlen(input); i++) {
+            if(i == strlen(input) - 1) { 
+                std::cout << input[i];
+                for(int j=2; j < strlen(input)+1; j++) {
+                    std::cout << input[strlen(input) - j];
+                }
+            }
+            else {
+                std::cout << input[i] << std::string(strlen(input) - 2, ' ') << input[strlen(input) - (i+1)] << std::endl;
+            }    
+        }
+    }
+    return 0;
+}
