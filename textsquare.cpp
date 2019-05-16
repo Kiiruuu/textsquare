15fb185ea1d0db8a84664ce5953dad81ddd58d60
Kiiruuu <anonymous.kiiruuu@gmail.com>
2019-05-16 08:56:00 +0800

    Update textsquare.cpp

--- textsquare.cpp
+++ textsquare.cpp
@@ -3,12 +3,12 @@
 
 int main() { 
     char input[46];
-    std::cout << "Type anything of up to 46 characters: ";
+    std::cout << "Type anything of up to 45 characters w/ no space character: ";
     std::cin >> input;
     std::cout << "\n\n";
 
     std::cout << input << "\n";
-    if(strlen(input) <= 46)  {
+    if(strlen(input) <= 45)  {
         for(int i=1; i < strlen(input); i++) {
             if(i == strlen(input) - 1) { 
                 std::cout << input[i];
